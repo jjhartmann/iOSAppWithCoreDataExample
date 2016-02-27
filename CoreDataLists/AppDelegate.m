0e8f832865acdcd0b2e708fd4bce91a2395feac8
Jeremy J Hartmann <jhartman@sfu.ca>
2016-02-27 14:57:20 -0800

    Add: managedObjectContext to view controller.

--- CoreDataLists/AppDelegate.m
+++ CoreDataLists/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ViewController *coursesViewC = (ViewController *) self.window.rootViewController;
+    coursesViewC.managedObjectContext = self.managedObjectContext;
+    
     return YES;
 }
 
--- CoreDataLists/ViewController.h
+++ CoreDataLists/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UITableViewController
 
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
 
 @end
 

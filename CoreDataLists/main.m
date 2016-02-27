016c25239c5a7cd87119cbfccc2647a68ef8b8be
Jeremy J Hartmann <jhartman@sfu.ca>
2016-02-26 23:05:06 -0800

    Initial Commit

---
+++ CoreDataLists/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataLists
+//
+//  Created by Jeremy Hartmann on 2016-02-26.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

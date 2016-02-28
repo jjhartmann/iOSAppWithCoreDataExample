837b5f6a30c1ed48b5efed34fe8c5342eb51f277
Jeremy J Hartmann <jhartman@sfu.ca>
2016-02-27 21:20:36 -0800

    Add: method to change course information to new data.

--- CoreDataLists/DisplayEditViewController.m
+++ CoreDataLists/DisplayEditViewController.m
@@ -15,18 +15,39 @@
     [super viewDidLoad];
     
     self.titleField.text = [self.currentCourse title];
-    self.authorField.text = [self.currentCourse title];
+    self.authorField.text = [self.currentCourse author];
     
     NSDateFormatter *df = [[NSDateFormatter alloc] init];
     [df setDateFormat:@"yyyy-mm-dd"];
     self.dateField.text = [df stringFromDate:[self.currentCourse releaseDate]];
+    
+    self.doneButton.hidden = YES;
     
 }
 
 
 - (IBAction)doneEditing:(id)sender
 {
+    self.titleField.enabled = NO;
+    self.authorField.enabled = NO;
+    self.dateField.enabled = NO;
     
+    self.currentCourse.title = self.titleField.text;
+    self.currentCourse.author = self.authorField.text;
+    self.currentCourse.releaseDate = self.datePickerView.date;
+    
+    NSDateFormatter *df = [[NSDateFormatter alloc] init];
+    [df setDateFormat:@"yyyy-mm-dd"];
+    self.dateField.text = [df stringFromDate:[self.currentCourse releaseDate]];
+    
+    
+    self.titleField.borderStyle = UITextBorderStyleNone;
+    self.authorField.borderStyle = UITextBorderStyleNone;
+    self.dateField.hidden = NO;
+    self.datePickerView.hidden = YES;
+    
+    self.editButton.hidden = NO;
+    self.doneButton.hidden = YES;
 }
 
 - (IBAction)startEditing:(id)sender
